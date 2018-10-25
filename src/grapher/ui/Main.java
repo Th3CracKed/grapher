0b521dec665b907e757f5795ea51fb6bb0dad53a
Th3CracKed <abdodriowya@gmail.com>
2018-10-25 19:03:07 +0200

    Menu ajouter

--- src/grapher/ui/Main.java
+++ src/grapher/ui/Main.java
@@ -6,6 +6,7 @@ import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.stage.Stage;
@@ -13,10 +14,17 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -36,9 +44,7 @@ public class Main extends Application {
                 listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
                 ObservableList<String> selectionCourrante = listView.getSelectionModel().getSelectedItems();//se mets automatiquement a jour
                 GrapherCanvas grapher = new GrapherCanvas(functNames,selectionCourrante);
-                root.setCenter(grapher);
                 selectionCourrante.addListener((Observable o) -> {
-                    System.out.println("listner");
                     grapher.redraw();
                     System.out.println(listView.getSelectionModel().getSelectedItems());
                 });
@@ -46,7 +52,7 @@ public class Main extends Application {
                 Button addBtn = new Button("+");
                 addBtn.setPrefSize(40, 40);
                 addBtn.setOnAction((ActionEvent event) -> {
-                    System.out.println("Add");
+                    System.out.println("Add Button");
                     TextInputDialog inputDialog = new TextInputDialog("Expression");
                     inputDialog.setTitle("Expression");
                     inputDialog.setHeaderText("Nouvelle expression : ");
@@ -62,7 +68,7 @@ public class Main extends Application {
                 Button deleteBtn = new Button("-");
                 deleteBtn.setPrefSize(40, 40);
                 deleteBtn.setOnAction((ActionEvent event) -> {
-                    System.out.println("Delete");
+                    System.out.println("Delete Button");
                     selectionCourrante.forEach((functName) -> {
                         functNames.remove(functName);
                     });  
@@ -73,13 +79,43 @@ public class Main extends Application {
                 hbox.setPadding(new Insets(0, 0, 0, 50));//permet d'ajouter du padding (dans ce cas 50 a gauche)
                 hbox.getChildren().addAll(addBtn,deleteBtn);
                 ToolBar toolBar = new ToolBar(hbox);
-                
+                MenuItem addMenu = new MenuItem("Ajouter");
+                addMenu.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
+                addMenu.setOnAction((ActionEvent e) -> {
+                    System.out.println("Add Menu");
+                    TextInputDialog inputDialog = new TextInputDialog("Expression");
+                    inputDialog.setTitle("Expression");
+                    inputDialog.setHeaderText("Nouvelle expression : ");
+                    inputDialog.setContentText("expression(x)");
+
+                    Optional<String> resultat = inputDialog.showAndWait();
+
+                    resultat.ifPresent(functName -> {
+                        functNames.add(functName);
+                        grapher.setFunctions(functNames);//update grapher with new funct list
+                    });
+                });
+                MenuItem deleteMenu = new MenuItem("Supprimer");
+                deleteMenu.setAccelerator(new KeyCodeCombination(KeyCode.BACK_SPACE, KeyCombination.CONTROL_DOWN));
+                deleteMenu.setOnAction((ActionEvent e) -> {
+                    System.out.println("Delete Menu");
+                    selectionCourrante.forEach((functName) -> {
+                        functNames.remove(functName);
+                    });  
+                    grapher.setFunctions(functNames);
+                });
+                final Menu menu = new Menu("Expression");
+                menu.getItems().addAll(addMenu,new SeparatorMenuItem(),deleteMenu);
+                MenuBar menuBar = new MenuBar();
+                menuBar.getMenus().addAll(menu);
+                root.setTop(menuBar);
                 VBox vbox = new VBox(20);
                 vbox.getChildren().addAll(listView,toolBar);
 		SplitPane rootSplitPane = new SplitPane();
-                rootSplitPane.getItems().addAll(new Group(vbox),root);
+                rootSplitPane.getItems().addAll(new Group(vbox),grapher);
+                root.setCenter(rootSplitPane);
 		stage.setTitle("grapher");
-		stage.setScene(new Scene(rootSplitPane));
+		stage.setScene(new Scene(root));
 		stage.show();
 	}
 	public static void main(String[] args) {
